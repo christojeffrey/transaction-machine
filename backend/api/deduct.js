ce54dcdb2578417e155443184c33bb221a5a639a
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-26 20:01:02 +0700

    add vercel endpoint

---
+++ backend/api/deduct.js
@@ -0,0 +1,19 @@
+const { deductHandler } = require("./deductHandler");
+
+export async function POST(req, res) {
+  if (req.headers.authorization !== process.env.AUTHORIZATION) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  // TODO: get from body
+  let ownerNfcId = req.body.nfcId;
+  let amount = req.body.amount;
+  //   validate
+  if (!ownerNfcId || !amount) {
+    res.status(400).send("nfcId and amount are required");
+    return;
+  }
+
+  let respond = await deductHandler(ownerNfcId, amount);
+  res.send(respond);
+}
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 require("dotenv").config({ path: "./.env" });
+// parse application/json
+app.use(bodyParser.json());
 
 const { deductHandler } = require("./deductHandler");
-// print testing from .env
-console.log(process.env.TESTING);
 app.get("/", async (req, res) => {
+  res.send("hello world!");
+});
+
+app.post("/deduct", async (req, res) => {
+  //   validate authorization
+  if (req.headers.authorization !== process.env.AUTHORIZATION) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
   // TODO: get from body
-  let ownerNfcId = "1234";
-  let amount = -20000;
+  let ownerNfcId = req.body.nfcId;
+  let amount = req.body.amount;
+  //   validate
+  if (!ownerNfcId || !amount) {
+    res.status(400).send("nfcId and amount are required");
+    return;
+  }
 
   let respond = await deductHandler(ownerNfcId, amount);
   res.send(respond);
