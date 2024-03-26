6e84f84301fb2a35108b84de71c16cf5639b6fcd
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-26 20:37:09 +0700

    fix: i think wrong respond in vercel

--- backend/api/deduct.js
+++ backend/api/deduct.js
@@ -2,18 +2,16 @@ const { deductHandler } = require("../deductHandler");
 
 export async function POST(req, res) {
   if (req.headers.authorization !== process.env.AUTHORIZATION) {
-    res.status(401).send("Unauthorized");
-    return;
+    return new Response("Unauthorized", { status: 401 });
   }
   // TODO: get from body
   let ownerNfcId = req.body.nfcId;
   let amount = req.body.amount;
   //   validate
   if (!ownerNfcId || !amount) {
-    res.status(400).send("nfcId and amount are required");
-    return;
+    return new Response("nfcId and amount are required", { status: 400 });
   }
 
   let respond = await deductHandler(ownerNfcId, amount);
-  res.send(respond);
+  return new Response(respond);
 }
