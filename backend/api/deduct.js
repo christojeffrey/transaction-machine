819a0acecd3afa2b75cdbfab5c70ab3494f14ff7
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2024-03-26 20:54:46 +0700

    feat: fix vercel reading body and headers

--- backend/api/deduct.js
+++ backend/api/deduct.js
@@ -1,17 +1,17 @@
 const { deductHandler } = require("../deductHandler");
 
 export async function POST(req, res) {
-  if (req.headers.authorization !== process.env.AUTHORIZATION) {
+  if (req.headers.get("authorization") !== process.env.AUTHORIZATION) {
     return new Response("Unauthorized", { status: 401 });
   }
-  // TODO: get from body
-  let ownerNfcId = req.body.nfcId;
-  let amount = req.body.amount;
+  const body = await req.json();
+  let ownerNfcId = body.nfcId;
+  let amount = body.amount;
   //   validate
   if (!ownerNfcId || !amount) {
     return new Response("nfcId and amount are required", { status: 400 });
   }
 
   let respond = await deductHandler(ownerNfcId, amount);
-  return new Response(respond);
+  return new Response(JSON.stringify(respond));
 }
